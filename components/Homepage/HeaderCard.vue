649416fd734b413def64bc096497b9e7b2c1a24c
codesyariah122 <pujiermanto@gmail.com>
2022-02-20 10:05:55 +0700

    adding overflow x hiden global.scss

--- components/Homepage/HeaderCard.vue
+++ components/Homepage/HeaderCard.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
     <div v-if="$device.isDesktop" class="banner">
       <mdb-carousel
-        :interval="8000"
-        slide
-        :items="items"
-        controlls
-        indicators
-        fade
-        dark
-        class="jumbotron__home"
-        customClass="rounded"
+      :interval="8000"
+      slide
+      :items="items"
+      controlls
+      indicators
+      fade
+      dark
+      class="jumbotron__home"
+      customClass="rounded"
       >
-      </mdb-carousel>
+    </mdb-carousel>
       <b-container class="container">
         <b-row>
           <b-col>
@@ -27,9 +26,9 @@
                 nasional.
               </p>
               <b-button
-                class="my__btn-primary rounded"
-                @click="$router.push({ name: 'auth-registrasi' })"
-                >Daftar Sekarang</b-button
+              class="my__btn-primary rounded"
+              @click="$router.push({ name: 'auth-registrasi' })"
+              >Daftar Sekarang</b-button
               >
             </div>
           </b-col>
@@ -40,29 +39,28 @@
     <!-- If Mobile using jumbotron -->
     <div v-else>
       <b-jumbotron
-        class="jumbotron__home"
-        :style="`background-image: url(${bg});`"
+      class="jumbotron__home"
+      :style="`background-image: url(${bg});`"
       >
-        <b-container>
-          <b-row>
-            <b-col>
-              <h2 class="display-6 text-capitalize">selamat datang di PPKC</h2>
-              <p>
-                Pusat Pengembangan Kesehatan Caroulus (PPKC) menyelenggarakan
-                pelatihan di bidang kesehatan yang profesional, unggul dan
-                berlandaskan I-CARE serta mampu berdaya saing di tingkat
-                nasional.
-              </p>
-              <b-button
-                class="my__btn-primary rounded"
-                @click="$router.push({ name: 'auth-registrasi' })"
-                >Daftar Sekarang</b-button
-              >
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-jumbotron>
-    </div>
+      <b-container>
+        <b-row>
+          <b-col>
+            <h2 class="display-6 text-capitalize">selamat datang di PPKC</h2>
+            <p>
+              Pusat Pengembangan Kesehatan Caroulus (PPKC) menyelenggarakan
+              pelatihan di bidang kesehatan yang profesional, unggul dan
+              berlandaskan I-CARE serta mampu berdaya saing di tingkat
+              nasional.
+            </p>
+            <b-button
+            class="my__btn-primary rounded"
+            @click="$router.push({ name: 'auth-registrasi' })"
+            >Daftar Sekarang</b-button
+            >
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-jumbotron>
   </div>
 </template>
 
