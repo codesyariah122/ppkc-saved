a45e1c938f22cc8ae5d5ad91b7c30ae6b012070f
codesyariah122 <pujiermanto@gmail.com>
2022-04-19 01:40:01 +0700

    update homepage style & html structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,6 +37,10 @@
 			this.CheckToken()
 		},
 
+		created(){
+			this.WidgetChat()
+		},
+
 		mounted(){
 			this.UserProfileData()
 		},
@@ -60,6 +64,44 @@
 					})
 					.catch(err => console.log(err.response ? err.response : ''))
 				}
+			},
+
+			WidgetChat(){
+				let url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?82740';
+				let s = document.createElement('script');
+				s.type = 'text/javascript';
+				s.async = true;
+				s.src = url;
+				let cta_text = this.$device.isDesktop ? 'Admin RSI Sultan Agung' : ''
+				let options = {
+					"enabled":true,
+					"chatButtonSetting":{
+						"backgroundColor":"#004899",
+						"ctaText":'',
+						"borderRadius":"25",
+						"marginLeft":"20",
+						"marginBottom":"20",
+						"marginRight":"50",
+						"position":"left"
+					},
+					"brandSetting":{
+						"brandName":"PPKC Carolus",
+						"brandSubTitle":"Pusat Pelatihan Kesehatan Carolus",
+						"brandImg":"https://gitlab.com/nirwan/ppkc-portal/-/raw/new-update-restyle/assets/images/profile/call-center.jpg",
+						"welcomeText":"Halo, new member\nada yang bisa kami bantu ?\ncukup kirimkan pesan kepada kami sekarang untuk mendapatkan info lebih lanjut tentang pelatihan tenaga kesehatan PPKC Carolus.",
+						"messageText":"Halo, Admin PPKC ! kami butuh bantuan untuk mengikuti pelatihan PPKC Carolus  {{page_link}}",
+						"backgroundColor":"#004899",
+						"ctaText":"Start Chat",
+						"borderRadius":"25",
+						"autoShow":false,
+						"phoneNumber":"6281284359312"
+					}
+				};
+				s.onload = function() {
+					CreateWhatsappChatWidget(options);
+				};
+				let x = document.getElementsByTagName('script')[0];
+				x.parentNode.insertBefore(s, x);
 			}
 		},
 
